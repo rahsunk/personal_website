8643c6dc2de40f6b60be150e35e40595688e2e8b
rahsunk <rtkf0522@gmail.com>
2022-08-06 10:45:36 -0400

    Updated about page CSS

---
+++ about/main.js
@@ -0,0 +1,19 @@
+const navLines=document.getElementsByClassName('noline')
+
+for(let i=0;i<navLines.length;i++){
+    navLines[i].onmouseover=function(){
+        navLines[i].style.color="gray"
+    }
+    navLines[i].onmouseout=function(){
+        navLines[i].style.color="black"
+    }
+    navLines[i].onmousedown=function(){
+        navLines[i].style.color="blue"
+    }
+    navLines[i].onmouseup=function(){
+        navLines[i].style.color="black"
+    }
+}
+
+const about=document.getElementById('about')
+about.style.fontWeight="bold"
