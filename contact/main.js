8e02a06cfa565ad378f4b7bcd42b1ff8f7703ba4
rahsunk <rtkf0522@gmail.com>
2022-08-06 00:24:48 -0400

    Updated contact page

--- contact/main.js
+++ contact/main.js
@@ -17,3 +17,90 @@ for(let i=0;i<navLines.length;i++){
 
 const contact=document.getElementById('contact')
 contact.style.fontWeight="bold"
+
+const list=document.getElementById('list')
+
+let footerColor=0
+
+for(let i=0;i<data.length;i++){
+    let newDiv=document.createElement('div')
+    newDiv.style.display="flex"
+    newDiv.style.margin="0px"
+    newDiv.style.paddingBottom="10px"
+    newDiv.style.paddingTop="10px"
+
+    if(i%2==0){
+        newDiv.style.backgroundColor="white"
+    }
+    else{
+        newDiv.style.backgroundColor="#f2f2f2"
+    }
+    
+    let picDiv=document.createElement('div')
+    
+    let newPic=document.createElement('img')
+    newPic.src="images/"+data[i].picture+".jpg"
+    
+    let txtDiv=document.createElement('div')
+    txtDiv.style.display="flex"
+    txtDiv.style.flexDirection="column"
+
+    newName=document.createElement('h2')
+    newName.innerHTML=data[i].name
+
+    newMail=document.createElement('div')
+    newMail.style.margin="0px"
+    newMail.innerHTML="Email: "+data[i].email+"@gmail.com"
+    newMail.style.marginBottom="10px"
+
+    newLink=document.createElement('a')
+    newLink.href="https://www.linkedin.com/in/"+data[i].link
+    newLink.target="_blank"
+    newLink.innerHTML="Linkedin"
+    newLink.style.marginBottom="10px"
+
+    newGit=document.createElement('a')
+    newGit.href="https://github.com/"+data[i].github
+    newGit.target="_blank"
+    newGit.innerHTML="GitHub"
+    newGit.style.marginBottom="10px"
+    
+    newWeb=document.createElement('a')
+    newWeb.href="https://"+data[i].website+".github.io"
+    newWeb.target="_blank"
+    newSite=document.createElement('button')
+    newSite.innerHTML="Visit their website!"
+    newSite.style.borderRadius="10px"
+    newSite.style.border="none"
+    newSite.style.padding="10px"
+    newSite.style.fontWeight="bold"
+    newSite.style.color="white"
+    newSite.style.backgroundColor="#800020"
+    newSite.className="button_opacity"
+
+    newWeb.appendChild(newSite)
+    picDiv.appendChild(newPic)
+    txtDiv.append(newName,newMail,newLink,newGit,newWeb)
+    newDiv.append(picDiv,txtDiv)
+    list.appendChild(newDiv)
+    
+    buttonOpac=document.getElementsByClassName('button_opacity')
+    for(let i=0;i<buttonOpac.length;i++){
+        buttonOpac[i].onmouseover=function(){
+                buttonOpac[i].style.opacity=0.5
+            }
+            buttonOpac[i].onmouseout=function(){
+                buttonOpac[i].style.opacity=1
+            }
+    }
+    footerColor++
+}
+
+const footer=document.querySelector('footer')
+if(footerColor%2==0){
+    footer.style.backgroundColor="white"
+}
+else{
+    footer.style.backgroundColor="#f2f2f2"
+}
+console.log(footerColor)
