688d2ad4f181a4ac39b095ee219ec7d14cc8e965
rahsunk <rtkf0522@gmail.com>
2022-08-06 01:12:38 -0400

    Added responsive design

--- contact/main.js
+++ contact/main.js
@@ -28,6 +28,7 @@ for(let i=0;i<data.length;i++){
     newDiv.style.margin="0px"
     newDiv.style.paddingBottom="10px"
     newDiv.style.paddingTop="10px"
+    newDiv.className="new_div"
 
     if(i%2==0){
         newDiv.style.backgroundColor="white"
@@ -42,7 +43,7 @@ for(let i=0;i<data.length;i++){
     newPic.src="images/"+data[i].picture+".jpg"
     newPic.style.width="200px"
     newPic.style.border="2px black solid"
-    newPic.style.marginLeft="20px"
+    newPic.className="new_pic"
     
     let txtDiv=document.createElement('div')
     txtDiv.style.display="flex"
@@ -55,18 +56,21 @@ for(let i=0;i<data.length;i++){
     newMail.style.margin="0px"
     newMail.innerHTML="Email: "+data[i].email+"@gmail.com"
     newMail.style.marginBottom="10px"
+    newMail.className="link_align"
 
     newLink=document.createElement('a')
     newLink.href="https://www.linkedin.com/in/"+data[i].link
     newLink.target="_blank"
     newLink.innerHTML="Linkedin"
     newLink.style.marginBottom="10px"
+    newLink.className="link_align"
 
     newGit=document.createElement('a')
     newGit.href="https://github.com/"+data[i].github
     newGit.target="_blank"
     newGit.innerHTML="GitHub"
     newGit.style.marginBottom="10px"
+    newGit.className="link_align"
     
     newWeb=document.createElement('a')
     newWeb.href="https://"+data[i].website+".github.io"
@@ -80,6 +84,7 @@ for(let i=0;i<data.length;i++){
     newSite.style.color="white"
     newSite.style.backgroundColor="#800020"
     newSite.className="button_opacity"
+    newWeb.className="button_pos"
 
     newWeb.appendChild(newSite)
     picDiv.appendChild(newPic)
