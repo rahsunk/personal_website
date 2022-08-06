69b921ec3778e3ee48f7d901bf98f1f9f208bf55
rahsunk <rtkf0522@gmail.com>
2022-08-06 19:24:38 -0400

    First commit

---
+++ projects/data.js
@@ -0,0 +1,18 @@
+let data=[
+    owasimul={
+        picture:"owasimul",
+        name:"Owasimul Sidick",
+        email: "owasimuls",
+        link: "owasimul-sidick-146373239",
+        github:"OwasimulS",
+        website:"owasimuls"
+    },
+    rick={
+        picture:"rick",
+        name:"Richard Perez",
+        email:"",
+        link:"richard-perez-jr/",
+        github:"theVerusDev",
+        website:"theverusdev"
+    }
+]
