ab913bb9f570b6571eb0d862ac4a27e7644d2f5b
rahsunk <rtkf0522@gmail.com>
2023-01-25 00:32:57 -0500

    Initialized news page

---
+++ news/main.js
@@ -0,0 +1,20 @@
+const navLines=document.getElementsByClassName('noline')
+
+for(let i=0;i<navLines.length;i++){
+    navLines[i].onmouseover=function(){
+        navLines[i].style.color="gray"
+    }
+    navLines[i].onmouseout=function(){
+        navLines[i].style.color="black"
+    }
+    navLines[i].onmousedown=function(){
+        navLines[i].style.color="blue"
+    }
+    navLines[i].onmouseup=function(){
+        navLines[i].style.color="black"
+    }
+}
+
+const news=document.getElementById('news')
+news.style.fontWeight="bold"
+news.style.backgroundColor="#f2f2f2"
