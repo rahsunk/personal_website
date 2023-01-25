e3d3b262467793eb285cf6b3678017ad198ba983
rahsunk <rtkf0522@gmail.com>
2023-01-25 09:10:37 -0500

    Initialized data

--- news/data.js
+++ news/data.js
@@ -1,1 +1,69 @@
 //technical director Nov 6
+
+const data=[
+    personalVideo={
+        picture:"personalVideo",
+        picture2:"",
+        name:"Personal Video Completed!",
+        date:"2023.01.23",
+        desc:[
+            "A short 2-minute video of my life has been completed!",
+            "Go check it out on the home page or below!",
+        ],
+        link:"https://drive.google.com/file/d/1br9SRwZHQyekn2wRLZh7NCYivA8yiI-e/view?usp=share_link",
+        video:""
+    },
+    newYear={
+        picture:"2023",
+        picture2:"",
+        name:"Happy New Year!",
+        date:"2023.01.01",
+        desc:[
+            "2023 is upon us!",
+            "A life-changing year for me, personally, as I graduate high school and",
+            "enter the next chapter of my life."
+        ],
+        link:"",
+        video:""
+    },
+    nets={
+        picture:"barclays",
+        picture2:"withDad",
+        name:"Brooklyn Nets Game",
+        date:"2022.12.23",
+        desc:[
+            "Winning the 2nd prize from All Star Code's online coding competition: Challenge Zone,",
+            "I won 2 tickets to the Brookln Nets vs. Milwaukee Bucks game at the Barclays Center.",
+            "It was the first time in a while we watched a sports game live.",
+            "It was an unforgettable experience (Nets beat Bucks lol)"
+        ],
+        link:"",
+        video:""
+    },
+    techDirector={
+        picture:"techDirector",
+        picture2:"",
+        name:"Promotion to School TV Technical Director",
+        date:"2022.11.06",
+        desc:[
+            "I have been promoted from cameraman to technical director for the Scholars' Academy TV Production team.",
+            "I look forward to my new job of configuring all graphics and backdrops of the videos.",
+            "Check out our most recent production below!"
+        ],
+        link:"",
+        video:""
+    },
+    az={
+        picture:"az",
+        picture2:"firstVideo",
+        name:"Annoucing Arithmetic Zone!",
+        date:"2022.10.22",
+        desc:[
+            "Using my acquired math and coding language, I created a channel to teach these topics to anyone.",
+            "Come back every weekend for 2 videos on obscure arithmetic topics and website development.",
+            "Check out our most recent video below!"
+        ],
+        link:"",
+        video:"https://www.youtube.com/watch?v=s1bZnpRRCp4",
+    }
+]
